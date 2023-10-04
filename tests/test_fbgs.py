07b58cf64b6586c851ad97debc826416f8cb536c
wweijtje <85878088+wweijtje@users.noreply.github.com>
2023-10-04 09:46:13 +0200

    Add test files

---
+++ tests/test_fbgs.py
@@ -0,0 +1,21 @@
+import os
+
+import pytest
+from sdypy_sep005.sep005 import assert_sep005
+
+from sep005_io_fbgs.fbgs import read_fbgs
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+static_dir = os.path.join(current_dir, 'static')
+GOOD_FILES = os.listdir(os.path.join(static_dir, 'good'))
+
+@pytest.mark.parametrize("filename", GOOD_FILES)
+def test_compliance_sep005(filename):
+    """
+        Test the compliance with the SEP005 guidelines
+        """
+    file_path = os.path.join(static_dir, 'good', filename)
+    signals = read_fbgs(file_path)  # should already not crash here
+
+    assert len(signals) != 0  # Not an empty response
+    assert_sep005(signals)
